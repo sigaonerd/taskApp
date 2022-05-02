ba76b9f9276cd47763dfb3766c8241d363a82edf
Marcelo Souza <marcelo@sigaonerd.com.br>
2022-05-02 17:59:04 -0300

    Created basic endpoint with express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Marcelo Souza');
+const express = require('express');
+const app = express();
+
+app.get('/nome', (req, res) => {
+	res.status(200).send('Marcelo Souza Silva');
+});
+
+app.listen(4000);
